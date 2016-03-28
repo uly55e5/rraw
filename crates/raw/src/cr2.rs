9c6354f25c3a4853d8dbc0fbde86b7ee7b832e25
uly55e5 <uly55e5@web.de>
2016-03-28 12:20:43 +0200

    read cr2 header at once

--- crates/raw/src/cr2.rs
+++ crates/raw/src/cr2.rs
@@ -67,36 +67,35 @@ impl RawImage {
 fn read_header(&mut self,f: &mut File) -> Result<(),RawFileError> {
     
     if 0 != try!(f.seek(::std::io::SeekFrom::Start(0))) { return Err(RawFileError::Seek(0)) } ;
-    let mut bo : [u8; 2] = [0,0];       // Byte Order
-    try!(f.read(&mut bo));
+    let mut head = [0u8; 16];
+    try!(f.read(&mut head));
+    
+    let bo = &head[0..2]; // Byte order
     let s = try!(str::from_utf8(&bo));
     match s {
         "II" => self.byte_order = ByteOrder::Intel,
         "MM" => self.byte_order = ByteOrder::Motorola,
-        _    => return Err(RawFileError::FileFormat("Unknown byte order".to_string())) 
+        _    => return Err(RawFileError::FileFormat("Unknown byte order ".to_string()+s)) 
     }
-    if s != "II" { return Err(RawFileError::NotImplemented("Only Intel Byte Order!".to_string())) };
+    if s != "II" { return Err(RawFileError::NotImplemented("Only Intel Byte Order supported!".to_string())) };
     
-    let mut tm : [u8; 2] = [0; 2];         // Tiff Magic
-    try!(f.read(&mut tm));    
+    let mut tm = [0u8; 2];
+    tm.clone_from_slice(&head[2..4]);         // Tiff Magic
     if unsafe{ mem::transmute::<[u8;2],u16>(tm)} != 0x002a { return Err(RawFileError::FileFormat("Tiff Magic mismatch".to_string()))};
         
-    let mut to : [u8; 4] = [0; 4];        // Tiff Offset
-    try!(f.read(&mut to));    
+    let mut to = [ 0u8; 4];        // Tiff Offset
+    to.clone_from_slice(&head[4..8]);
     unsafe { self.tiff_offset = mem::transmute::<[u8;4],u32>(to)};
     
-    let mut cm : [u8; 2] = [0; 2];         // CR2 Magic
-    try!(f.read(&mut cm));    
+    let cm = &head[8..10];         // CR2 Magic
     if try!(str::from_utf8(&cm)) != "CR" { return Err(RawFileError::FileFormat("CR2 Magic mismatch".to_string()));}
     
-    let mut cmaj : [u8; 1] = [0];        // CR2 Major
-    try!(f.read(&mut cmaj));    
-    let mut cmin : [u8; 1] = [0];        // CR2 Minor
-    try!(f.read(&mut cmin));    
-    if (cmaj ,cmin) != ([2],[0]) { return Err(RawFileError::FileFormat("Version not 2.0".to_string()));}
+    let cmaj = &head[10..11];        // CR2 Major
+    let cmin = &head[11..12];        // CR2 Minor
+    if cmaj[0]!=2 && cmin[0]!=0 { return Err(RawFileError::NotImplemented(format!("CR2 Version {}.{} not supported",cmaj[0],cmin[0])));}
     
-    let mut io : [u8; 4] = [0;4];         // IFD Offset
-    try!(f.read(&mut io));    
+    let mut io = [0u8;4];         // IFD Offset
+    io.clone_from_slice(&head[12..16]);
     unsafe { self.ifd_offset = mem::transmute::<[u8;4],u32>(io)};
     Ok(())
 
